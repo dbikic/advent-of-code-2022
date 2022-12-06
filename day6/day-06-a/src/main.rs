6b12199f74c65a62a7a880a4b356d970b9c20c4a
Dino Bikic <dino.bikic@websummit.com>
2022-12-06 09:10:00 +0100

    Day 6

---
+++ day6/day-06-a/src/main.rs
@@ -0,0 +1,32 @@
+use std::collections::HashSet;
+
+fn main() {
+    include_str!("../input.txt")
+        .lines()
+        .into_iter()
+        .for_each(|x| {
+            let chars: Vec<char> = x.chars().collect();
+            for i in 0..chars.len() {
+                for j in 3..chars.len() {
+                    if j < chars.len() {
+                        if are_different(&chars[i], &chars[i + 1], &chars[i + 2], &chars[i + 3]) {
+                            println!("Index = {}", i + 4);
+                            return;
+                        }
+                    } else {
+                        return;
+                    }
+                }
+            }
+        }
+        );
+}
+
+fn are_different(c0: &char, c1: &char, c2: &char, c3: &char) -> bool {
+    let mut set: HashSet<&char> = HashSet::new();
+    set.insert(c0);
+    set.insert(c1);
+    set.insert(c2);
+    set.insert(c3);
+    set.len() == 4
+}
---
+++ day6/day-06-b/src/main.rs
@@ -0,0 +1,31 @@
+use std::collections::HashSet;
+
+fn main() {
+    include_str!("../input.txt")
+        .lines()
+        .into_iter()
+        .for_each(|x| {
+            let chars: Vec<char> = x.chars().collect();
+            for i in 0..chars.len() {
+                for j in 13..chars.len() {
+                    if j < chars.len() {
+                        if are_different(&chars[i..i + 14]) {
+                            println!("Index = {}", i + 14);
+                            return;
+                        }
+                    } else {
+                        return;
+                    }
+                }
+            }
+        }
+        );
+}
+
+fn are_different(vec: &[char]) -> bool {
+    let mut set: HashSet<char> = HashSet::new();
+    for x in vec {
+        set.insert(x.clone());
+    }
+    vec.len() == set.len()
+}
