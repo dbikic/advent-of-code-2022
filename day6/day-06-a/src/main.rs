a74bd85db94192491a63bcf7fdd3284cab1d771f
Dino Bikic <dino.bikic@websummit.com>
2022-12-06 10:01:46 +0100

    Upgrade day6

--- day6/day-06-a/src/main.rs
+++ day6/day-06-a/src/main.rs
@@ -7,28 +7,13 @@ fn main() {
         .lines()
         .into_iter()
         .for_each(|x| {
-            let chars: Vec<char> = x.chars().collect();
-            for i in 0..chars.len() {
-                for j in 3..chars.len() {
-                    if j < chars.len() {
-                        if are_different(&chars[i], &chars[i + 1], &chars[i + 2], &chars[i + 3]) {
-                            println!("Index = {}", i + 4);
-                            return;
-                        }
-                    } else {
-                        return;
-                    }
+            for i in 0..x.len() {
+                let chars: HashSet<char> = x[i..i + 4].chars().collect();
+                if chars.len() == 4 {
+                    println!("Index = {}", i + 4);
+                    return;
                 }
             }
         }
         );
-}
-
-fn are_different(c0: &char, c1: &char, c2: &char, c3: &char) -> bool {
-    let mut set: HashSet<&char> = HashSet::new();
-    set.insert(c0);
-    set.insert(c1);
-    set.insert(c2);
-    set.insert(c3);
-    set.len() == 4
 }
--- day6/day-06-b/src/main.rs
+++ day6/day-06-b/src/main.rs
@@ -7,27 +7,13 @@ fn main() {
         .lines()
         .into_iter()
         .for_each(|x| {
-            let chars: Vec<char> = x.chars().collect();
-            for i in 0..chars.len() {
-                for j in 13..chars.len() {
-                    if j < chars.len() {
-                        if are_different(&chars[i..i + 14]) {
-                            println!("Index = {}", i + 14);
-                            return;
-                        }
-                    } else {
-                        return;
-                    }
+            for i in 0..x.len() {
+                let chars: HashSet<char> = x[i..i + 14].chars().collect();
+                if chars.len() == 14 {
+                    println!("Index = {}", i + 14);
+                    return;
                 }
             }
         }
         );
-}
-
-fn are_different(vec: &[char]) -> bool {
-    let mut set: HashSet<char> = HashSet::new();
-    for x in vec {
-        set.insert(x.clone());
-    }
-    vec.len() == set.len()
 }
